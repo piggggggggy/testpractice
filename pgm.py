197da4bf29596ad204e62183dce4c65f20fc2e42
piggggggggy <pyt4105@gmail.com>
2021-05-25 23:23:40 +0900

    testprac3.0

--- pgm.py
+++ pgm.py
@@ -1,16 +1,68 @@
-#프로그래머스 그리디 2번문제!!!(미완성)
-def solution(name):
-    alp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-    # 알파벳 조정
-    count = 0
-    for i in name:
-        idx = alp.index(i)
-        if idx <= 13:
-            count += idx
+
+#
+# def solution(name):
+# 위아래@@
+
+    # name = 'AABBBBSSADAABA'
+    # alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # updown = 0
+    # for spel in name:
+    #     num = alphabet.index(spel)
+    #     if num < 13:
+    #         updown += num
+    #     else:
+    #         updown += (26-num)
+
+    # updown : A->? 조이스틱 횟수의 합
+
+# 좌우@@
+    N = len(name)
+
+    # 1. 오른쪽으로만 갈 경우   'AXXAAXXA' 라고 했을 때 가장 마지막에 있는 X의 index값
+    i=1
+    while i < N:
+        if name[N-i] != 'A':
+            break
+        i +=1
+    right = N - i
+
+#     # 2. 왼쪽으로만 갈 경우
+    for j in range(N):
+        if name[j+1] == 'A':
+            pass
         else:
-            count += (26-idx)
+            break
+    left = N - (j+1)
 
-    #좌우조정
-
-    return answer
+#     # 3. 오른쪽갔다가 왼쪽으로 갈 경우
+#     lst = []
+#     t = 0
+#     for l in name:
+#         if l != 'A':
+#             lst.append(t)
+#         t += 1
+#
+#
+#     rl = []
+#     le = len(lst)
+#     if le >1:
+#         if lst[0] != 0:
+#             for q in range(le-1):
+#                 o = (2*lst[q]+N-lst[q+1])
+#                 rl.append(o)
+#         else:
+#             for q in range(1,le-1):
+#                 o = (2 * lst[q] + N - lst[q + 1])
+#                 rl.append(o)
+#     else:
+#         for m in lst:
+#             o = m
+#             rl.append(o)
+#
+#     # 1,2,3의 경우 중 최소값과 위아래 최소값의 합
+#     answer = min(right,left,min(rl))+updown
+#     return print(answer)
+#
+# aaaa = 'JADDZ'
+# solution(aaaa)
--- prac.py
+++ prac.py
@@ -1,16 +1,126 @@
 #코드업 16진법 만들기
 
-a = input()
+# a = input()
+#
+# sixt = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
+# for k,v in sixt.items():
+#     if v == a:
+#         p=k
+#
+# for i in range(1,16):
+#     m = p*i//16
+#     r = p*i%16
+#     if p*i<16 :
+#         print(a+"*"+sixt[i]+"="+a)
+#     else:
+#         print(a+"*"+sixt[i]+"="+sixt[m]+sixt[r])
 
-sixt = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
-for k,v in sixt.items():
-    if v == a:
-        p=k
+# a = {'a':5,'b': 7, 'c':4}
+#
+# b = list(map(int, a))
+#
+# print(b)
 
-for i in range(1,16):
-    m = p*i//16
-    r = p*i%16
-    if p*i<16 :
-        print(a+"*"+sixt[i]+"="+a)
-    else:
-        print(a+"*"+sixt[i]+"="+sixt[m]+sixt[r])
+
+# 3. 오른쪽갔다가 왼쪽으로 갈 경우
+
+
+
+    #
+    # name = 'BBDAEFADA'
+    #
+    #
+    #
+    # N = len(name)
+    #
+    # # 위아래
+    # alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # updown = 0
+    # for i in name:
+    #     p = alp.index(i)
+    #     if p < 13:
+    #         updown += p
+    #     else:
+    #         updown += (26 - p)
+    # # 좌우
+    # # 1. 오른쪽으로만 갈 경우
+    # a = 1
+    # for i in range(N):
+    #     if name[N - a] == 'A':
+    #         a += 1
+    #     else:
+    #         break
+    # right = N - a
+    #
+    # # 2. 왼쪽으로만 갈 경우
+    # b = 0
+    # for j in name:
+    #     if j == 'A':
+    #         b += 1
+    #     else:
+    #         break
+    # left = N - b
+    #
+    # lst = []
+    # t = 0
+    # for l in name:
+    #     if l != 'A':
+    #         lst.append(t)
+    #     t += 1
+    #
+    # rl = []
+    # le = len(lst)
+    # if le > 1:
+    #     if lst[0] != 0:
+    #         for q in range(le - 1):
+    #             o = (2 * lst[q] + N - lst[q + 1])
+    #             rl.append(o)
+    #     else:
+    #         for q in range(1, le - 1):
+    #             o = (2 * lst[q] + N - lst[q + 1])
+    #             rl.append(o)
+    # else:
+    #     for m in lst:
+    #         o = m
+    #         rl.append(o)
+    # answer = min(right,left,min(rl))+updown
+    # print(answer)
+
+
+# name = 'JAZAAAABBA'
+# N=len(name)
+#
+# for j in range(N):
+#     if name[j + 1] == 'A':
+#         pass
+#     else:
+#         break
+# left = N - (j + 1)
+#
+# print(left)
+# a = [0]
+# print(min(a))
+
+
+name = 'AABBBBSSADAABA'
+# alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# updown = 0
+# for spel in name:
+#     num = alphabet.index(spel)
+#     if num < 13:
+#         updown += num
+#     else:
+#         updown += (26-num)
+#
+# print(updown)
+
+
+N = len(name)
+# 1. 오른쪽으로만 갈 경우   'AXXAAXXA' 라고 했을 때 가장 마지막에 있는 X의 index값
+i=1
+while i < N:
+    if name[N - i] != 'A':
+        break
+    i += 1
+right = N - i
+print(right)
