6711536303564d6c03d7b9c45ec352f0cf9f3ef6
piggggggggy <pyt4105@gmail.com>
2021-05-25 23:28:27 +0900

    testprac210525

--- pgm.py
+++ pgm.py
@@ -36,33 +36,33 @@
     left = N - (j+1)
 
 #     # 3. 오른쪽갔다가 왼쪽으로 갈 경우
-#     lst = []
-#     t = 0
-#     for l in name:
-#         if l != 'A':
-#             lst.append(t)
-#         t += 1
-#
-#
-#     rl = []
-#     le = len(lst)
-#     if le >1:
-#         if lst[0] != 0:
-#             for q in range(le-1):
-#                 o = (2*lst[q]+N-lst[q+1])
-#                 rl.append(o)
-#         else:
-#             for q in range(1,le-1):
-#                 o = (2 * lst[q] + N - lst[q + 1])
-#                 rl.append(o)
-#     else:
-#         for m in lst:
-#             o = m
-#             rl.append(o)
-#
-#     # 1,2,3의 경우 중 최소값과 위아래 최소값의 합
-#     answer = min(right,left,min(rl))+updown
-#     return print(answer)
-#
-# aaaa = 'JADDZ'
-# solution(aaaa)
+    lst = []
+    t = 0
+    for l in name:
+        if l != 'A':
+            lst.append(t)
+        t += 1
+
+
+    rl = []
+    le = len(lst)
+    if le >1:
+        if lst[0] != 0:
+            for q in range(le-1):
+                o = (2*lst[q]+N-lst[q+1])
+                rl.append(o)
+        else:
+            for q in range(1,le-1):
+                o = (2 * lst[q] + N - lst[q + 1])
+                rl.append(o)
+    else:
+        for m in lst:
+            o = m
+            rl.append(o)
+
+    # 1,2,3의 경우 중 최소값과 위아래 최소값의 합
+    answer = min(right,left,min(rl))+updown
+    return print(answer)
+
+aaaa = 'JADDZ'
+solution(aaaa)
