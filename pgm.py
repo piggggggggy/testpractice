3bba158e8f919359d5fc5e454c303ef3c077a6a4
piggggggggy <pyt4105@gmail.com>
2021-05-27 03:48:29 +0900

    testprac210527

--- pgm.py
+++ pgm.py
@@ -1,79 +1,33 @@
-
 def solution(name):
     N = len(name)
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    updown = 0
-    for spel in name:
-        num = alphabet.index(spel)
-        if num < 13:
-            updown += num
-        else:
-            updown += (26-num)
+    alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    def alphabet(spel):
+        num = alph.index(spel)
+        if num > 13:
+            num = 26-num
+        return num
+    updown = sum(map(alphabet,[i for i in name]))
+    # A가 아닌 알파벳들의 index list
+    lst = [i for i,v in enumerate(name) if v != 'A']
+    # 우 이동
+    right = lst[-1]
+    # 좌 이동
+    left = N-lst[1] if lst[0]==0 else N-lst[0]
+    # 왼->오
+    if lst[0]==0:
+        lst.remove(0)
+    NN = len(lst)
+    def countlr(index):
+        return 2*lst[index]+N-lst[index+1]
+    if NN > 1:
+        leftright = min(map(countlr, [j for j in range(NN-1)]))
 
-    i = 1
-    while i < N:
-        if name[N-i] != 'A':
-            break
-        i +=1
-    right = N - i
+        lst.reverse()
+        lst = list(map(lambda x:N-x, lst))
 
-    j = 1
-    while j < N:
-        if name[j] != 'A':
-            break
-        j += 1
-    left = N - j
+        rightleft = min(map(countlr, [k for k in range(NN-1)]))
 
-    lst = []
-    for i, v in enumerate(name):
-        if v != 'A':
-            lst.append(i)
-
-    # 3-1. 오른쪽 갔다가 왼쪽
-    rightleft = 0
-    if len(lst) > 1 and lst[0] != 0:
-        k = 0
-        while k < len(lst) - 1:
-            move = 2 * lst[k] + N - lst[k + 1]
-            # rightleft.append(move)
-            if rightleft == 0 or move < rightleft:
-                rightleft = move
-            k += 1
-    elif len(lst) > 1 and lst[0] == 0:
-        l = 1
-        while l < len(lst) - 1:
-            move = 2 * lst[l] + N - lst[l + 1]
-            # rightleft.append(move)
-            if rightleft == 0 or move < rightleft:
-                rightleft = move
-            l += 1
-
-    leftright = 0
-    if len(lst) > 1 and lst[0] != 0:
-        k = -1
-        while -k < len(lst):
-            move = 2 * (N - lst[k]) + lst[k - 1]
-            # leftright.append(move)
-            if leftright == 0 or move < leftright:
-                leftright = move
-            k -= 1
-    elif len(lst) > 1 and lst[0] == 0:
-        k = -1
-        while -k < len(lst)-1:
-            move = 2 * (N - lst[k]) + lst[k - 1]
-            # leftright.append(move)
-            if leftright == 0 or move < leftright:
-                leftright = move
-            k -= 1
-
-    if leftright>0 and rightleft>0:
-        mini = min(right, left, leftright,rightleft)
-    elif leftright == 0 and rightleft>0:
-        mini = min(right, left, rightleft)
-    elif rightleft == 0 and leftright >0:
-        mini = min(right, left, leftright)
+        answer = min(right, left, rightleft, leftright) + updown
     else:
-        mini = min(right,left)
-
-    answer = mini + updown
+        answer = min(right,left)+updown
     return answer
