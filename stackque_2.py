c23411846df90586328d731bc767b66610810d98
piggggggggy <pyt4105@gmail.com>
2021-06-03 17:19:14 +0900

    testprac210603

--- stackque_2.py
+++ stackque_2.py
@@ -1,17 +1,19 @@
 def solution(priorities, location):
-    k = priorities[location]
-    priorities[location] = k * 0.9
-    count = 0
+    priorities[location] = k = priorities[location] * 1.1
+    count = len([i for i in priorities if i > k])
     while True:
-        if max(priorities) == k or max(priorities) == k * 0.9:
+        if max(priorities) == k:
             break
         elif priorities[0] > k:
             priorities.pop(0)
-            count += 1
         else:
             priorities.append(priorities.pop(0))
-
-    prior = [i for i in priorities if i >= k * 0.9]
-    count_ = prior.index(k * 0.9) + 1
+    prior = [j for j in priorities if j >= k/1.1]
+    count_ = prior.index(k) + 1
     answer = count + count_
     return answer
+
+a = [2, 1, 3, 2]
+b = 2
+
+print(solution(a,b))
--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 
-priorities = [1, 1, 9, 1, 1, 1]
-location = 0
+priorities = [4,3,5,4,7,7,7,8,8,8,2,1,5,3,5,2,5,6]
+location = 12
 
-k = priorities[location]
-priorities[location] = k * 0.9
 
-count = 0
+priorities[location] = k = priorities[location] * 1.1
+
+count = len([i for i in priorities if i > k])
+print(count)
+
+
 while True:
-    if max(priorities) == k or max(priorities) == k * 0.9:
+    if max(priorities) == k:
         break
-
     elif priorities[0] > k:
         priorities.pop(0)
-        count += 1
     else:
         priorities.append(priorities.pop(0))
+print(priorities)
 
-prior = [i for i in priorities if i>=k*0.9]
+prior = [j for j in priorities if j >= k/1.1]
+print(prior)
+count_ = prior.index(k) + 1
 
-count_ = prior.index(k*0.9)+1
+print(count_)
 answer = count + count_
+
 print(answer)
